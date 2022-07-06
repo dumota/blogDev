f492bfa4cc071b79c7587dee6b9f99523ef4f6cf
Dumota <edumota02@gmail.com>
2022-07-06 10:02:09 -0300

    setup config

---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+//Middleware
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+//routes
+app.get('/',(req, res)=>{
+    res.json({msg: 'hello mota first route is functional'})
+})
+
+//Database
+
+import './config/database';
+
+//server Listening
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log('server is runnig on port', PORT);
+    
+})
